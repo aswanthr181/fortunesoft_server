01333f44478e94c9d391f93224a2b45e3dca1b26
aswanth <aswanthr18tech@gmail.com>
2024-05-13 14:51:50 +0530

    aggregation and auth errors

--- controller/userController.js
+++ controller/userController.js
@@ -3,31 +3,59 @@ const movie=require('../model/movieSchema')
 const fetchmovies=async(req,res)=>{
     console.log('fetchmoie');
     try {
-        const movies=await movie.find({})
+        // const movies=await movie.find({})
 
-        const groupedMovies = {};
-        movies.forEach((movie) => {
-            movie.genres.forEach((genre) => {
-                if (!groupedMovies[genre]) {
-                    groupedMovies[genre] = [];
+        // const groupedMovies = {};
+        // movies.forEach((movie) => {
+        //     movie.genres.forEach((genre) => {
+        //         if (!groupedMovies[genre]) {
+        //             groupedMovies[genre] = [];
+        //         }
+        //         groupedMovies[genre].push({
+        //             director: movie.director,
+        //             imdb_rating: movie.imdb_rating,
+        //             length: movie.length,
+        //             poster: movie.poster,
+        //             title: movie.title,
+        //             slug: movie.slug
+        //         });
+        //     });
+        // });
+
+        // const formattedMovies = Object.keys(groupedMovies).map((genre) => {
+        //     return {
+        //         category: genre,
+        //         movies: groupedMovies[genre]
+        //     };
+        // });
+        // res.json({ status: true, result: formattedMovies });
+        const formattedMovies = await movie.aggregate([
+            {
+                $unwind: "$genres"
+            },
+            {
+                $group: {
+                    _id: "$genres",
+                    movies: {
+                        $push: {
+                            director: "$director",
+                            imdb_rating: "$imdb_rating",
+                            length: "$length",
+                            poster: "$poster",
+                            title: "$title",
+                            slug: "$slug"
+                        }
+                    }
                 }
-                groupedMovies[genre].push({
-                    director: movie.director,
-                    imdb_rating: movie.imdb_rating,
-                    length: movie.length,
-                    poster: movie.poster,
-                    title: movie.title,
-                    slug: movie.slug
-                });
-            });
-        });
-
-        const formattedMovies = Object.keys(groupedMovies).map((genre) => {
-            return {
-                category: genre,
-                movies: groupedMovies[genre]
-            };
-        });
+            },
+            {
+                $project: {
+                    _id: 0,
+                    category: "$_id",
+                    movies: 1
+                }
+            }
+        ]);
         res.json({ status: true, result: formattedMovies });
 
     } catch (error) {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,29 @@
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-const verifyToken=async(req,res,next)=>{
-    let token=req.header("Authorization")
+const verifyToken = async (req, res, next) => {
+    let token = req.header("Authorization")
     try {
-        if(!token){
-            return res.status(404).json({message:"authorization failed: no token provided"})
+        if (!token) {
+            return res.status(404).json({ message: "authorization failed: no token provided" })
         }
-        if(token.startsWith("Bearer ")){
+        if (token.startsWith("Bearer ")) {
+            token = token.slice(7, token.length).trimLeft()
             console.log(token);
-            
-            next();
+            if (token === 'fs1234') {
+                next();
+            } else {
+                return res.status(401).json({ message: "Authorization failed: invalid token" });
+            }
+        } else {
+            return res.status(401).json({ message: "Authorization failed: invalid token format" });
         }
     } catch (error) {
         return res
-      .status(404)
-      .json({ message: "Authentication failed: invalid token." });
+            .status(404)
+            .json({ message: "Authentication failed: invalid token." });
     }
 }
 
-module.exports={
+module.exports = {
     verifyToken
 }
